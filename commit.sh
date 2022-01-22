729f04011c28276983676ea5ebeb25d850aa89b2
Bruno Pedro <bpedro@brunopedro.com>
2022-01-22 19:47:49 +0100

    Update content

--- commit.sh
+++ commit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd $SCRIPT_DIR
+cd "$SCRIPT_DIR"
 git add .
 git commit -m "Update content"
 git push origin main
