3d53a1ec09ccaae3170a6703085a89b7bb0c8c68
Bruno Pedro <bpedro@brunopedro.com>
2022-01-25 18:17:52 +0100

    Fix typo

--- commit.sh
+++ commit.sh
@@ -2,7 +2,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR"
 
-% convert each untracked markdown note to html
+# convert each untracked markdown note to html
 for file in $(git ls-files --others --exclude-standard | grep ".md"); do
   base=`echo ${file} | sed 's/\.md$//'`
   html=${base}.html
