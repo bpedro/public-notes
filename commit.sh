6d0d6951d955efe69a2ce77d90f6f48597a2be58
Bruno Pedro <bpedro@brunopedro.com>
2022-01-25 18:15:27 +0100

    Update content

--- commit.sh
+++ commit.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR"
+
+% convert each untracked markdown note to html
+for file in $(git ls-files --others --exclude-standard | grep ".md"); do
+  base=`echo ${file} | sed 's/\.md$//'`
+  html=${base}.html
+  pandoc --self-contained -s ${file} -o ${html}
+done
+
 git add .
 git commit -m "Update content"
 git push origin main
