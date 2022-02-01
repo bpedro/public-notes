e1eeb3fa79e417e454b58ba1c141f46bceb2ab70
Bruno Pedro <bpedro@brunopedro.com>
2022-02-01 18:29:55 +0100

    Automatically generate a tag index

--- commit.sh
+++ commit.sh
@@ -2,6 +2,37 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR"
 
+# get all file names and contents
+contents=()
+files=$(find ./2022 -name "*.md")
+for file in ${files}; do
+  OLDIFS=$IFS
+  IFS=""
+  contents+=($file $(<${file}))
+  IFS=$OLDIFS
+done
+
+# get a unique list of tags
+all_tags=()
+for ((i=0; i<${#contents[@]}; i+=2)); do
+  content=${contents[$i+1]}
+  all_tags+=($(echo "$content" | grep 'Tags' | grep -o '#[^#]*' | sed 's/#//'))
+done
+all_tags=($(echo ${all_tags[@]} | tr ' ' '\n' | sort -u | tr '\n' ' '))
+
+# generate the index
+echo "# Tag index" > tags.md
+for tag in ${all_tags[@]}; do
+  echo "## ${tag}" >> tags.md
+  for ((i=0; i<${#contents[@]}; i+=2)); do
+    content=${contents[$i+1]}
+    if [[ $content =~ .*Tags.*${tag}.* ]]; then
+      base=`echo ${contents[$i]} | sed 's/\.md$//' | sed 's/^.*\///'`
+      echo "* [${base}](${contents[$i]})" >> tags.md
+    fi
+  done
+done
+
 # convert each untracked markdown note to html
 for file in $(git ls-files --others --exclude-standard | grep ".md"); do
   base=`echo ${file} | sed 's/\.md$//'`
@@ -9,6 +40,7 @@ for file in $(git ls-files --others --exclude-standard | grep ".md"); do
   pandoc --self-contained -s ${file} -o ${html}
 done
 
+# push changes to github
 git add .
 git commit -m "Update content"
 git push origin main
