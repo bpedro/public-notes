f3836db2ae9608fe47161046e5e54b36c2504a99
Bruno Pedro <bpedro@brunopedro.com>
2022-05-24 17:38:18 +0200

    Conditionally set the date based on the running platform

--- gen_pdf.sh
+++ gen_pdf.sh
@@ -2,6 +2,15 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR"
 
+# obtain platform where the script is running
+platform='unknown'
+unamestr=$(uname)
+if [ "$unamestr" = 'Linux' ]; then
+   platform='linux'
+elif [ "$unamestr" = 'FreeBSD' ] || [ "$unamestr" = 'Darwin' ]; then
+   platform='bsd'
+fi
+
 # get arguments from command line
 if (( $# == 0 )); then
   echo "Usage: $0 <input markdown file>"
@@ -13,11 +22,15 @@ pdf=${base}.pdf
 
 # get date
 [[ $filename =~ \.\/([0-9]{4}\/[0-9]{2}\/[0-9]{2})\/ ]] && date=${BASH_REMATCH[1]}
-
+if [ "$platform" = 'linux' ]; then
+  date=$(date -d$date +"%A, %B %-d, %Y")
+elif [ "$platform" = 'bsd' ]; then
+  date=$(date -j -f%Y/%m/%d $date +"%A, %B %-d, %Y")
+fi
 echo "---
 title: Public note
 author: Bruno Pedro
-date: $(date -j -f%Y/%m/%d $date +"%A, %B %-d, %Y")
+date: $date
 ---
 $(cat ${filename})
 " | \
